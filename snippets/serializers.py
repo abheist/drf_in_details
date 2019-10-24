b03144f6d09c31c6f2f6363dcc1e1e941c17b55c
Abhishek <abhishek291093@gmail.com>
2019-10-24 14:27:39 +0530

    AKS | converted views into viewsets and wiredup the urls

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,9 +1,21 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['url', 'id', 'highlight', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,11 +1,40 @@
 from django.urls import path
+from rest_framework import renderers
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_class=[renderers.StaticHTMLRenderer])
+
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('', views.api_root),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
