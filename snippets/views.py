27bbb5566a67fb2555023b960f6e8c4bbe4c7bbf
Abhishek <abhishek291093@gmail.com>
2019-10-24 14:45:12 +0530

    AKS | fix the highlight issue

--- snippets/views.py
+++ snippets/views.py
@@ -30,6 +30,7 @@ class SnippetViewSet(viewsets.ModelViewSet):
     @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
+        print('======================', snippet)
         return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
